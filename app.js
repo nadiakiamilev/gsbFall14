100745587da66e749ce453b7dca3006a4e13ff6e
nadiakiamilev <nadia.kiamilev@gmail.com>
2014-11-23 16:51:48 -0500

    removed http:

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ var MongoClient = require('mongodb').MongoClient;
 var mongoose = require('mongoose');
 
 /*DB Code*/
-var dbconnection = process.env.MONGODBPROD || 'localhost:27017/mattressmovers'
+console.log(process.env.MONGODBPROD)
+var dbconnection = process.env.MONGODBPROD || 'mongodb://localhost:27017/mattressmovers'
 mongoose.connect(dbconnection)
 var db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error'))
