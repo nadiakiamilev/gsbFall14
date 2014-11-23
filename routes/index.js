8a532a02a045166c6e405bfd7314e10cfe3117fa
nadiakiamilev <nadia.kiamilev@gmail.com>
2014-11-23 18:07:56 -0500

    changed title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'MATTRESS MOVERS' });
+  res.render('index', { title: 'Mattress Movers' });
   console.log('hey, we responded with our index page');
 });
 
